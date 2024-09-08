a63ae87fc0cd2bfeac1d46e716372a3d93b037c4
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-09-08 10:58:54 -0300

    setup: primevue

---
+++ front-end/nuxt.config.ts
@@ -0,0 +1,25 @@
+import Aura from '@primevue/themes/aura'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    "@primevue/nuxt-module"
+  ],
+  $production: {
+    routeRules: {
+      '/**': { isr: true }
+    }
+  },
+  primevue: {
+    autoImport: true,
+    options: {
+      theme: {
+          preset: Aura
+      }
+    }
+  }
+
+
+})
