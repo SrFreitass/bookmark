0e490e3342c643c7671fabaa36568e6c09148ace
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-03 00:39:52 -0400

    feat: add table and chart in dashboard

---
+++ front-end/components/borrowTable.vue
@@ -0,0 +1,15 @@
+<template>
+    <DataTable :value="borrows">
+        <Column field="user" header="Usuário"></Column>
+        <Column field="book" header="Livro"></Column>
+        <Column field="category" header="Categoria"></Column>
+        <Column field="createdAt" header="Data de saída"></Column>
+        <Column field="limitDate" header="Data de entrega"></Column>
+    </DataTable>
+</template>
+
+<script setup lang="ts">
+    const { borrows  } = defineProps([
+        "borrows"
+    ])
+</script>
--- front-end/pages/admin/dashboard.vue
+++ front-end/pages/admin/dashboard.vue
@@ -7,14 +7,35 @@
             <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
             <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
             <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
+            <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
         </div>
+        <BorrowTable :borrows="[exampleBorrow]" class="mt-6"/>
+        <Chart type="bar" :data="exampleChartData"/>
     </main>
 </template>
 
 <script setup lang="ts">
 import Card from '~/components/card.vue';
+    const exampleBorrow = {
+        user: 'Grelo',
+        book: 'Entendo Algoritmos',
+        category: 'Computação',
+        createdAt: new Date().toLocaleDateString('pt-BR'),
+        limitDate: new Date().toLocaleDateString('pt-BR')
+    }
+
 
     const user = {
         name: 'Guilherme Freitas'
+    }
+
+    const exampleChartData = {
+        labels: ['Contos', 'Romance', 'Literatura negra', 'Educação'],
+        datasets: [
+            {
+                label: 'Empréstimos',
+                data: [20, 10, 5, 30]
+            }
+        ]
     }
 </script>
