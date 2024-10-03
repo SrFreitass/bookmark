1cc99f48cd4d30eb56ecadbdbc22570e8b2acabd
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-03 00:59:34 -0400

    feat: add sidebar in dashboard (static)

---
+++ front-end/components/sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+    <Menu  class="w-80 !rounded-none !border-t-0" :model="nav" />
+</template>
+
+<script setup lang="ts">
+    const nav = [
+        {   
+            class: 'mt-5 text-lg px-4',
+            icon: 'pi pi-home',
+            label: 'Início'
+        },
+        {   
+            class: 'mt-5 text-lg px-4',
+            icon: 'pi pi-bookmark',
+            label: 'Emprestimos'
+        },
+        {   
+            class: 'mt-5 text-lg px-4',
+            icon: 'pi pi-book',
+            label: 'Livros'
+        },
+        {   
+            class: 'mt-5 text-lg px-4',
+            icon: 'pi pi-users',
+            label: 'Usuários'
+        },
+    ]
+</script>
--- front-end/pages/admin/dashboard.vue
+++ front-end/pages/admin/dashboard.vue
@@ -1,21 +1,27 @@
 <template>
     <Header/>
-    <main class="p-6">
-        <p>Dashboard</p>
-        <h1 class="text-2xl">Bom dia, {{ user.name }}!</h1>
-        <div class="flex gap-8">
-            <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
-            <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
-            <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
-            <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
-        </div>
-        <BorrowTable :borrows="[exampleBorrow]" class="mt-6"/>
-        <Chart type="bar" :data="exampleChartData"/>
-    </main>
+    <div class="flex min-h-screen">
+        <Sidebar/>
+        <main class="p-6">
+            <p>Dashboard</p>
+            <h1 class="text-2xl">Bom dia, {{ user.name }}!</h1>
+            <div class="flex gap-8">
+                <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
+                <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
+                <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
+                <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
+            </div>
+            <div class="flex gap-8 mt-6">
+                <BorrowTable :borrows="[exampleBorrow, exampleBorrow, exampleBorrow, exampleBorrow, exampleBorrow, exampleBorrow, exampleBorrow]" class="w-1/2"/>
+                <Chart type="bar" :data="exampleChartData" class="w-1/2"/>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script setup lang="ts">
 import Card from '~/components/card.vue';
+import Sidebar from '~/components/sidebar.vue';
     const exampleBorrow = {
         user: 'Grelo',
         book: 'Entendo Algoritmos',
