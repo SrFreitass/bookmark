109dc57556f4a275ba8fcb8f28bb547a53bc4093
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-03 00:18:21 -0400

    feat: add card in dashboard

---
+++ front-end/components/card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-between w-[22rem] h-32 border border-[#5454547a] p-4 rounded-lg mt-6">
+        <div>
+            <h3 class="text-xl font-semibold">{{ title }}</h3>
+            <p class="text-2xl font-semibold mt-2">{{ value  }}</p>
+            <p>{{ description  }}</p>
+        </div>
+        <div class="border border-[#54545471] rounded-xl w-10 h-10 flex justify-center items-center">
+            <i :class="`${icon} `"></i>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const { icon, title, value, description } = defineProps([
+        "icon",
+        "title",
+        "value",
+        "description"
+    ]);
+</script>
---
+++ front-end/pages/admin/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <Header/>
+    <main class="p-6">
+        <p>Dashboard</p>
+        <h1 class="text-2xl">Bom dia, {{ user.name }}!</h1>
+        <div class="flex gap-8">
+            <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
+            <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
+            <Card title="Empréstimos" icon="pi pi-bookmark" value="54" description="Quantidade de emprestimos p/mês"/>
+        </div>
+    </main>
+</template>
+
+<script setup lang="ts">
+import Card from '~/components/card.vue';
+
+    const user = {
+        name: 'Guilherme Freitas'
+    }
+</script>
