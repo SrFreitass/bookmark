bfd9deee61e2dfb380ef9320e5c4e616a8cbfdb5
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-04 16:36:24 -0400

    feat: books grid component and books page

---
+++ front-end/components/booksGrid.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-wrap gap-6 mt-4">
+        <div v-for="book in books" class="flex flex-col max-w-48 gap-2">
+            <NuxtLink :href="`./edit/${book.id}`">
+                <img :src="book.coverURL" :alt="`Capa do livro ${book.title}`" class="w-48 h-64 object-cover">
+                <h3>{{ book.title }}</h3>
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const { books } = defineProps<{
+        books: {
+            id: string;
+            title: string;
+            coverURL: string;
+        }[]
+    }>();
+</script>
---
+++ front-end/pages/admin/books.vue
@@ -0,0 +1,24 @@
+<template>
+    <Header />
+    <div class="flex min-h-screen">
+        <Sidebar />
+        <main class="p-6 w-full">
+            <p>Acervo</p>
+            <h1 class="text-2xl font-semibold">Livros</h1>
+            <BooksGrid :books="books"/>
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Sidebar from '~/components/sidebar.vue';
+    const books: { id: string, title: string; coverURL: string }[] = [];
+
+    for(let i = 0; i < 25; i++) {
+        books.push({
+            id: 'abc123',
+            title: 'Entendendo Algoritmos',
+            coverURL: 'https://m.media-amazon.com/images/I/71Vkg7GfPFL._AC_UF1000,1000_QL80_.jpg'
+        })
+    }
+</script>
