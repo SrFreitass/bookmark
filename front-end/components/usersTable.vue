d4fc99dcc65822bc73bb8fe32d9e393ad2adc833
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-04 17:06:03 -0400

    feat: add users table in admin/users page

---
+++ front-end/components/usersTable.vue
@@ -0,0 +1,16 @@
+<template>
+    <DataTable show-gridlines class="mt-4":value="[{ username: 'Freitasdev', name: 'Guilherme F.', role: 'Desenvolvedor', email: 'freitas@google.com', createdAt: '01/01/2025' }]">
+        <Column key="username" header="Usuário">
+            <template #body="user">
+                <div class="flex items-center gap-2">
+                    <img src="https://github.com/srfreitass.png" width="42px" height="42px" class="rounded-md"/>
+                    <p>{{ user.data.username }}</p>
+                </div>
+            </template>
+        </Column>
+        <Column key="name" header="Nome" field="name"></Column>
+        <Column key="role" header="Cargo" field="role"></Column>
+        <Column key="email" header="E-mail" field="email"></Column>
+        <Column key="createdAt" header="Criada" field="createdAt"></Column>
+    </DataTable>
+</template>
---
+++ front-end/pages/admin/users.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Sidebar from '~/components/sidebar.vue';
+</script>
+
+<template>
+    <Header/>
+    <div class="flex min-h-screen">
+        <Sidebar/>
+        <main class="p-6 w-full">
+            <p>Gerencie todos os usuários da biblioteca</p>
+            <h1 class="text-2xl font-semibold">Gerenciar usuários</h1>
+            <UsersTable /> 
+        </main>
+    </div>
+</template>
