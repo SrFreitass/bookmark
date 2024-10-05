806a1dd9c218c342b2e7939ea6d44ed76729d8de
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-05 13:14:48 -0400

    feat: filter users in admin/users page

---
+++ front-end/components/filterUsers.vue
@@ -0,0 +1,31 @@
+<template>
+    <InputGroup class="mt-4">
+        <InputGroupAddon>
+            <i class="pi pi-filter" @click="toggle" aria-controls="overlay_options" aria-haspopup="true"></i>
+        </InputGroupAddon>
+        <InputText placeholder="Pesquisar usuário ou nome"/>
+    </InputGroup>
+    <Menu ref="menu" :popup="true" id="overlay_options" :model="filterOptions" class="!mt-5 -ml-3" />
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const menu = ref();
+
+    const toggle = (e) => {
+        menu.value.toggle(e);
+    }
+
+    const filterOptions = [
+        {
+            icon: 'pi pi-exclamation-circle',
+            label: 'Devedores'
+        },
+        {
+            icon: 'pi pi-spinner',
+            label: 'Pendentes'
+        },
+    ]
+
+</script>
--- front-end/pages/admin/users.vue
+++ front-end/pages/admin/users.vue
@@ -9,6 +9,7 @@ import Sidebar from '~/components/sidebar.vue';
         <main class="p-6 w-full">
             <p>Gerencie todos os usuários da biblioteca</p>
             <h1 class="text-2xl font-semibold">Gerenciar usuários</h1>
+            <FilterUsers />
             <UsersTable /> 
         </main>
     </div>
