64cba52ee79fc8dfbbe250cccca39a1d75f99d56
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-07 10:16:28 -0400

    refactor: book carousel improvements

--- front-end/components/bookCarousel.vue
+++ front-end/components/bookCarousel.vue
@@ -2,12 +2,14 @@
     <div class="p-8">
         <h2 class="text-2xl font-semibold mb-4"><NuxtLink :href="`/category/${label.category}`">{{ label.title }}</NuxtLink></h2>
         <Swiper
-        :slides-per-view="10"
-        :spaceBetween="24"
+        :slides-per-view="'auto'"
+        :space-between="32"
         >
-            <SwiperSlide v-for="book in books" :key="book.id">
-                <img :src="book.coverURL" class="rounded-lg"/>
-                <p class="text-slate-200">{{ book.title }}</p>
+            <SwiperSlide v-for="book in books" :key="book.id" class="!w-44">
+                <div>
+                    <img :src="book.coverURL" class="rounded-lg max-w-44"/>
+                    <p class="text-slate-200">{{ book.title }}</p>
+                </div>
             </SwiperSlide>
         </Swiper>
     </div>
