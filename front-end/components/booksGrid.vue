f34abed284cccaaac54d6e045a87e6ce93e3f8a8
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-08 06:53:04 -0400

    feat: favorites books user page

--- front-end/components/booksGrid.vue
+++ front-end/components/booksGrid.vue
@@ -2,20 +2,23 @@
     <div class="flex flex-wrap gap-8 mt-4">
         <div v-for="book in books" class="flex flex-col max-w-48 gap-2">
             <NuxtLink :href="book.url">
-                <img :src="book.coverURL" :alt="`Capa do livro ${book.title}`" class="w-48 h-64 object-cover">
-                <h3>{{ book.title }}</h3>
+                <img :src="book.coverURL" :alt="`Capa do livro ${book.title}`" class="w-48 h-64 object-cover rounded-md">
+                <p class="text-end"><i v-if="iconButton && onClickBook" :class="iconButton" class="absolute -ml-10 -mt-10 text-3xl text-green-400" @click="onClickBook"></i></p>
+                <h3 class="mt-2 text-gray-100">{{ book.title }}</h3>
             </NuxtLink>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    const { books } = defineProps<{
+    const { books, iconButton, onClickBook } = defineProps<{
         books: {
             id: string;
             title: string;
             coverURL: string;
             url: string;
-        }[]
+        }[];
+        iconButton?: string;
+        onClickBook?: () => void;
     }>();
 </script>
---
+++ front-end/pages/user/favorites.vue
@@ -0,0 +1,25 @@
+<template>
+    <Header />    
+    <main class="flex gap-4">
+        <UserSidebar/>
+        <div class="mt-8">
+            <h2 class="text-2xl font-semibold">Livros favoritados</h2>
+            <p class="text-gray-400">21 livros favoritados</p>
+            <!-- <hr class="border-border"/> -->
+            <BooksGrid :books="books" icon-button="pi pi-heart-fill" v-on:click-book="() => console.log('Desfavoritado')" />
+        </div>
+    </main>
+</template>
+
+<script setup lang="ts">
+    const books: { id: string, title: string; coverURL: string, url: string }[] = [];
+
+    for(let i = 0; i < 21; i++) {
+        books.push({
+            id: 'abc123',
+            title: 'Entendendo Algoritmos',
+            coverURL: 'https://m.media-amazon.com/images/I/71Vkg7GfPFL._AC_UF1000,1000_QL80_.jpg',
+            url: `?editBook=abc123`
+        })
+    }
+</script>
