474eb2e4a375e0697588a97699e801e5e3910d62
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-08 21:59:40 -0400

    feat: user notifications page

---
+++ front-end/components/notification.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mt-4 p-4 border border-border rounded-lg flex flex-col gap-4">
+        <div class="flex gap-2 items-center">
+            <i class="pi pi-exclamation-circle text-xl"></i>
+            <p>{{ message }}</p>
+        </div>
+        <time class="text-gray-400">{{ datetime }}</time>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const { datetime, message } = defineProps<{
+        message: string;
+        datetime: string;
+    }>()
+</script>
---
+++ front-end/pages/user/notifications.vue
@@ -0,0 +1,18 @@
+<template>
+    <Header/>
+    <main class="flex gap-4 w-full">
+        <UserSidebar/>
+        <div class="w-[80%] mt-8">
+            <h2 class="text-2xl font-semibold">Notificações</h2>
+            <Notification message="Você tem 7 dias pra devolver o entendendo algoritmos" datetime="01/01/2025 às 16h"/>
+            <Notification message="Você tem 6 dias pra devolver o entendendo algoritmos" datetime="01/01/2025 às 16h"/>
+            <Notification message="Você tem 5 dias pra devolver o entendendo algoritmos" datetime="01/01/2025 às 16h"/>
+            <Notification message="Você tem 4 dias pra devolver o entendendo algoritmos" datetime="01/01/2025 às 16h"/>
+        </div>
+    </main>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
