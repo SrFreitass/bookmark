c0d98f6e480d98d482b92ef12062157972b11c51
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-09 11:01:39 -0400

    wip: category/[category] page

---
+++ front-end/pages/category/[category].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p class="text-gray-400">Todos livros da categoria {{ category }}</p>
+        <h2 class="text-xl font-semibold">{{ category }}</h2>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const route = useRoute()
+    let category = route.params.category.toString();
+
+    // Capitalize
+    category = `${category.charAt(0).toUpperCase()}${category.substring(1, category.length)}`;
+</script>
