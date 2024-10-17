c5ee6db98fe4ae402383ef955ed9af0640ff4567
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-17 06:09:17 -0400

    feat: get borrows with from, to (back-end)

--- back-end/src/infra/repositories/borrowBook.repository.ts
+++ back-end/src/infra/repositories/borrowBook.repository.ts
@@ -1,4 +1,4 @@
-import { and, eq, or } from "drizzle-orm";
+import { and, between, count, eq, lte, or } from "drizzle-orm";
 import { BorrowBookEntity } from "../../core/domains/entities/borrowBook.entity";
 import { BorrowBookRepository } from "../../core/repositories/IBorrowBook.repository";
 import { db } from "../db/connect";
@@ -11,6 +11,32 @@ class BorrowBookRepositoryImpl implements BorrowBookRepository {
         private readonly database: typeof db,
         private readonly borrowBook: BorrowBook
     ) {}
+    async countBorrowBooks(filter?: { borrow?: boolean; }): Promise<Number> {
+        if (filter?.borrow) {
+            const total = await this.database.select({
+                total: count()
+            }).from(this.borrowBook)
+            .where(eq(this.borrowBook.borrow, filter.borrow))
+
+            return total[0].total;
+        }
+
+        const total = await this.database.select({
+            total: count()
+        }).from(this.borrowBook);
+
+        return total[0].total;
+    }
+
+    
+
+    async findBorrowBooks({ from, to }: { from: Date; to: Date }): Promise<BorrowBookEntity[]> {
+        return await this.database.query.borrowBooks.findMany({
+            where: and(
+                between(this.borrowBook.createdAt, from, to),
+            )
+        });
+    }
 
     async findBorrowBook(fields: { userId?: string; bookId?: string; borrow: boolean }): Promise<BorrowBookEntity | null> {
         const borrowBook = await this.database.query.borrowBooks.findFirst({
