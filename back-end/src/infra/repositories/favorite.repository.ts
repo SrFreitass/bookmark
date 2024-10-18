ec8b270fdc42e67044ed71d6cb4b6c2ab68e4844
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-18 01:09:01 -0400

    feat: add count favorites books by user id

--- back-end/src/infra/repositories/favorite.repository.ts
+++ back-end/src/infra/repositories/favorite.repository.ts
@@ -1,10 +1,14 @@
-import { and, eq } from "drizzle-orm";
+import { and, count, eq } from "drizzle-orm";
 import { FavoriteEntity } from "../../core/domains/entities/favorite.entity";
 import { FavoriteRepository } from "../../core/repositories/IFavorite.repository";
 import { db } from "../db/connect";
 import { favoritiesBooks } from "../db/schema";
 
 class FavoriteRepositoryImpl implements FavoriteRepository {
+    async countFavorite(userId: string): Promise<number> {
+        return (await db.select({ total: count() }).from(favoritiesBooks).where(eq(favoritiesBooks.userId, userId)))[0].total;
+    }
+
     async findFavorite(userId: string, bookId: string): Promise<FavoriteEntity[]> {
         return await db.select().from(favoritiesBooks).where(and(eq(favoritiesBooks.userId, userId), eq(favoritiesBooks.bookId, bookId)))
     }
