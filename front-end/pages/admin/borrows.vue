d1fe042b11b10dbf1737a87642f38cfe96eea6bb
GustavoFreiree <gustavostallmann@gmail.com>
2024-10-17 23:02:52 -0300

    refactor: reorganize componets

--- front-end/pages/admin/borrows.vue
+++ front-end/pages/admin/borrows.vue
@@ -1,47 +1,39 @@
 <template>
-    <div>
-        <p>Emprestimos</p>
-        <h1 class="text-2xl font-semibold">Overview de emprestimos</h1>
-        <div class="mt-4">
-            <CreateBorrowModal />
-        </div>
-        <div class="mt-6 flex flex-col gap-4">
-            <SearchBorrows/>
-            <BorrowTable :borrows="borrows"/>
-        </div>
-        <Toast position="bottom-right" />
+  <div>
+    <p>Emprestimos</p>
+    <h1 class="text-2xl font-semibold">Overview de emprestimos</h1>
+    <div class="mt-4">
+      <BooksBorrowModal />
     </div>
+    <div class="mt-6 flex flex-col gap-4">
+      <SearchBorrows />
+      <BorrowTable :borrows="borrows" />
+    </div>
+    <Toast position="bottom-right" />
+  </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
-    layout: 'admin'
+  layout: "admin",
 });
-import BorrowTable from '~/components/borrowTable.vue';
-import CreateBorrowModal from '~/components/createBorrowModal.vue';
-import SearchBorrows from '~/components/searchBorrows.vue';
-import Sidebar from '~/components/adminSidebar.vue';
-import { getBorrows } from '~/http/borrow/getBorrows';
-import type { IBorrow } from '~/models/IBorrow';
-
 const borrows = reactive<{ list: IBorrow[] }>({
-    list: [],
+  list: [],
 });
 
 const fetchBorrows = async () => {
-    console.log('fetching borrows');
-    const res = await getBorrows();
+  console.log("fetching borrows");
+  const res = await getBorrows();
 
-    if(!res?.success) {
-        console.log('error fetching borrows');
-        return;
-    }
+  if (!res?.success) {
+    console.log("error fetching borrows");
+    return;
+  }
 
-    console.log('borrows fetched');
-    console.log(res.data);
-    borrows.list = res.data;
-}
+  console.log("borrows fetched");
+  console.log(res.data);
+  borrows.list = res.data;
+};
 
 fetchBorrows();
-
 </script>
--- front-end/pages/book/[bookId].vue
+++ front-end/pages/book/[bookId].vue
@@ -1,46 +1,48 @@
 <template>
-    <main class="flex flex-col max-w-[1600px] justify-center h-[80vh] mx-32 m-auto">
-        <div class="flex gap-2 items-center mb-8">
-            <i class="pi pi-arrow-left"></i>
-            <p>Voltar</p>
-        </div>
-        <BookInfo :book="book"/>
-    </main>
-    <BookCarousel 
-        class="max-w-[1600px] m-auto p-0"
-        :label="{ category: '', title: 'Livro similares' }" 
-        :books="books"
-    />
-    <!-- <BookCarousel :label="{ title: 'Livro similares', category: '#' }"/> -->
+  <main
+    class="flex flex-col max-w-[1600px] justify-center h-[80vh] mx-32 m-auto"
+  >
+    <div class="flex gap-2 items-center mb-8">
+      <i class="pi pi-arrow-left"></i>
+      <p>Voltar</p>
+    </div>
+    <BookInfo :book="book" />
+  </main>
+  <BookCarousel
+    class="max-w-[1600px] m-auto p-0"
+    :label="{ category: '', title: 'Livro similares' }"
+    :books="books"
+  />
+  <!-- <BookCarousel :label="{ title: 'Livro similares', category: '#' }"/> -->
 </template>
 
 <script lang="ts" setup>
-import BookCarousel from '~/components/bookCarousel.vue';
-import BookInfo from '~/components/bookInfo.vue';
-import Header from '~/components/header.vue';
-    const books: { 
-        id: string;
-        title: string;
-        coverURL: string; 
-    }[] = []
+const books: {
+  id: string;
+  title: string;
+  coverURL: string;
+}[] = [];
 
-    for(let i = 0; i < 25; i++) {
-        books.push({ 
-            id: '2b',
-            coverURL: 'https://m.media-amazon.com/images/I/81IGFC6oFmL._AC_UF1000,1000_QL80_.jpg',
-            title: 'Design Patterns: Elements of Reusable Object Oriented Software'
-        })
-    }
+for (let i = 0; i < 25; i++) {
+  books.push({
+    id: "2b",
+    coverURL:
+      "https://m.media-amazon.com/images/I/81IGFC6oFmL._AC_UF1000,1000_QL80_.jpg",
+    title: "Design Patterns: Elements of Reusable Object Oriented Software",
+  });
+}
 
-    const book = {
-        coverURL: 'https://m.media-amazon.com/images/I/71Vkg7GfPFL._AC_UF1000,1000_QL80_.jpg', 
-        title: 'Entendendo algoritmos',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, dignissimos quis? Dolores eaque aliquid reiciendis repellat illo tenetur asperiores aperiam maxime ducimus? Ipsam odit, cum atque tempora fugiat qui et!',
-        category: 'Computação',
-        authors: ['John Doe', 'Grelo da Silva Neto', 'Fabio Akita'],
-        publisher: 'Novatec',
-        pages: 250,
-        publishedAt: 2022,
-        isbn: '978xxxxxxxxxx'
-    }
+const book = {
+  coverURL:
+    "https://m.media-amazon.com/images/I/71Vkg7GfPFL._AC_UF1000,1000_QL80_.jpg",
+  title: "Entendendo algoritmos",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, dignissimos quis? Dolores eaque aliquid reiciendis repellat illo tenetur asperiores aperiam maxime ducimus? Ipsam odit, cum atque tempora fugiat qui et!",
+  category: "Computação",
+  authors: ["John Doe", "Grelo da Silva Neto", "Fabio Akita"],
+  publisher: "Novatec",
+  pages: 250,
+  publishedAt: 2022,
+  isbn: "978xxxxxxxxxx",
+};
 </script>
