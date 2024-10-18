ee0d645f809abfd4178e958430e3198bda844a01
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-10-18 01:13:36 -0400

    feat: add countFavorit method in favoriteRepository

--- back-end/src/core/repositories/IFavorite.repository.ts
+++ back-end/src/core/repositories/IFavorite.repository.ts
@@ -1,6 +1,7 @@
 import { FavoriteEntity } from "../domains/entities/favorite.entity";
 
 interface FavoriteRepository {
+    countFavorite(userId: string): Promise<number>;
     addFavorite(favoriteEntity: FavoriteEntity): Promise<void>;
     removeFavorite(userId: string, bookId: string): Promise<void>;
     findFavorite(userId: string, bookId: string): Promise<FavoriteEntity[]>;
