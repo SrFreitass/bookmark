7de95548b14201bddb7c9f586925a926de848f7c
SrFreitass <guilhermefreitasdonascimento67@gmail.com>
2024-11-20 20:39:37 -0400

    fix: get favorite book (http request)

--- front-end/http/favorities/getFavoriteBook.ts
+++ front-end/http/favorities/getFavoriteBook.ts
@@ -1,17 +1,15 @@
 import { client } from "../client";
 import type { HTTPResponse } from "../types/http.response";
 
-const getFavoriteBook = async (bookId: string): Promise<HTTPResponse<{ 
-    id: string, 
-    bookId: string, 
-    userId: string }> 
+const getFavoriteBook = async (bookId: string): Promise<HTTPResponse<{
+    id: string,
+    bookId: string,
+    userId: string }>
     | null> => {
     try {
         const { value: { user } } = useGlobalState();
 
-        return await client("GET", `/favorite/${bookId}`, null, 'application/json', {
-            'userid': user?.id || '',
-        });
+        return await client("GET", `/favorite/${bookId}`);
     } catch(err) {
         console.error(err);
         return null;
